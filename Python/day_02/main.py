80f709219303a84de342bb2297a5ad30ad2cf4b7
arielbeje <arielbeje@protonmail.com>
2022-12-02 12:04:31 +0200

    python: day_02: Solve part 2
    Just remembered that Python now has pattern matching

--- Python/day_02/main.py
+++ Python/day_02/main.py
@@ -1,6 +1,11 @@
 with open("input.txt") as f:
     day_input = f.read().rstrip("\n")
 
+WIN_POINTS = 6
+TIE_POINTS = 3
+LOSS_POINTS = 0
+
+# All of the matrices are just shifts of one another
 EQUALITY_MATRIX = {
     "A": "X",
     "B": "Y",
@@ -14,6 +19,12 @@ WIN_MATRIX = {
     "C": "X",
 }
 
+LOSS_MATRIX = {
+    "A": "Z",
+    "B": "X",
+    "C": "Y",
+}
+
 POINTS_AWARDED = {
     "X": 1,
     "Y": 2,
@@ -21,16 +32,29 @@ POINTS_AWARDED = {
 }
 
 
-def score_round(opponents_play: str, our_play: str) -> int:
+def score_round_part_1(opponents_play: str, our_play: str) -> int:
     our_play_points = POINTS_AWARDED[our_play]
 
     if WIN_MATRIX[opponents_play] == our_play:
-        return our_play_points + 6
+        return our_play_points + WIN_POINTS
 
     if EQUALITY_MATRIX[opponents_play] == our_play:
-        return our_play_points + 3
+        return our_play_points + TIE_POINTS
 
-    return our_play_points
+    return our_play_points + LOSS_POINTS
 
 
-print(sum(score_round(*row.split(" ")) for row in day_input.splitlines()))
+def score_round_part_2(opponents_play: str, our_play: str) -> int:
+    match our_play :
+        case "Z":
+            return POINTS_AWARDED[WIN_MATRIX[opponents_play]] + WIN_POINTS
+
+        case "Y":
+            return POINTS_AWARDED[EQUALITY_MATRIX[opponents_play]] + TIE_POINTS
+
+        case "X":
+            return POINTS_AWARDED[LOSS_MATRIX[opponents_play]] + LOSS_POINTS
+
+    raise ValueError("Invalid move for our_play")
+
+print(sum(score_round_part_2(*row.split(" ")) for row in day_input.splitlines()))
